08a9f89e9a7158446a1beccbe4c2b9f3b147beb7
Weslley Miranda <contato@weslleymiranda.dev>
2026-08-17 14:33:24 -0300

    Update listening message to include URL

--- asmx/core.asm
+++ asmx/core.asm
@@ -29,7 +29,7 @@ extern server_fd, client_fd, asmx_handler
 extern buffer, route
 
 section .data
-    msg_listen db "asmx: listening on "
+    msg_listen db "asmx: listening on http://localhost:"
     msg_listen_len equ $ - msg_listen
     msg_nl db 10
 
