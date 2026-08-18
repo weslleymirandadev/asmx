dc2c8c386ada60394e270f92ad7888e0648c5079
weslleymirandadev <contato@weslleymirandadev>
2026-08-18 20:54:03 -0300

    ui: read_file_to (read into an arbitrary buffer)

--- ui/io.asm
+++ ui/io.asm
@@ -5,51 +5,73 @@
 ; read_file(rdi = path) -> rax = 0 ok / -1 err; fills in_buf/in_len
 ; ----------------------------------------------------------------------
 read_file:
+    push r12
+    mov r12, rdi
+    lea rsi, [in_buf]
+    mov rdx, IN_CAP
+    call read_file_to
+    test rax, rax
+    js .err
+    mov [in_len], rax
+    xor rax, rax
+    pop r12
+    ret
+.err:
+    mov rax, -1
+    pop r12
+    ret
+
+; read_file_to(rdi = path, rsi = buf, rdx = cap) -> rax = len or -1
+; ----------------------------------------------------------------------
+read_file_to:
     push rbx
     push r12
     push r13
+    push r14
+    mov r13, rsi                ; buf
+    mov rbx, rdx                ; cap
     mov rax, SYS_open
     mov rsi, O_RDONLY
     syscall
     test rax, rax
     js .err
     mov r12, rax                ; fd
-    xor r13, r13                ; pos
+    xor r14, r14                ; pos
 .read:
-    cmp r13, IN_CAP
+    cmp r14, rbx
     jge .full
     mov rax, SYS_read
     mov rdi, r12
-    lea rsi, [in_buf + r13]
-    mov rdx, IN_CAP
-    sub rdx, r13
+    lea rsi, [r13 + r14]
+    mov rdx, rbx
+    sub rdx, r14
     syscall
     test rax, rax
     jz .done
     js .err_read
-    add r13, rax
+    add r14, rax
     jmp .read
 .full:
-    lea rdi, [msg_big]
-    mov rsi, msg_big_len
-    call die
-.err_read:
-    mov r13, -1
+    mov r14, -1
     jmp .close
+.err_read:
+    mov r14, -1
 .done:
-    mov [in_len], r13
-    xor r13, r13                ; ok
+    mov rax, r14
 .close:
+    push rax
     mov rax, SYS_close
     mov rdi, r12
     syscall
-    mov rax, r13
+    pop rax
+    pop r14
     pop r13
     pop r12
     pop rbx
     ret
 .err:
     mov rax, -1
+    pop r14
     pop r13
     pop r12
     pop rbx
